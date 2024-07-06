d03116bc71ad23ad2f9b4d842955a240791a2aef
Bullets-jyr <jyr@bullets.co.kr>
2024-07-07 05:13:36 +0900

    feat: Implicitly Animated Widgets
    Closes: #FA-6

--- lib/screens/implicit_animations_screen.dart
+++ lib/screens/implicit_animations_screen.dart
@@ -4,12 +4,55 @@ class ImplicitAnimationsScreen extends StatefulWidget {
   const ImplicitAnimationsScreen({super.key});
 
   @override
-  State<ImplicitAnimationsScreen> createState() => _ImplicitAnimationsScreenState();
+  State<ImplicitAnimationsScreen> createState() =>
+      _ImplicitAnimationsScreenState();
 }
 
 class _ImplicitAnimationsScreenState extends State<ImplicitAnimationsScreen> {
+  bool _visible = true;
+
+  void _trigger() {
+    setState(() {
+      _visible = !_visible;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Implicit Animations'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedAlign(
+              alignment: _visible ? Alignment.topLeft : Alignment.topRight,
+              duration: Duration(seconds: 2),
+              child: AnimatedOpacity(
+                opacity: _visible ? 1 : 0.2,
+                duration: Duration(seconds: 2),
+                child: Container(
+                  width: size.width * 0.8,
+                  height: size.width * 0.8,
+                  color: Colors.amber,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: _trigger,
+              child: const Text(
+                'Go!',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
